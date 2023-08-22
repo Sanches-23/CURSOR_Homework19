49b7e34903fe297ac184dfd3b2b4b35be82bb8e5
Sanches-23 <pasechnik23.2002@gmail.com>
2023-08-22 21:58:36 +0300

    hw19

--- src/components/pages/photos/Photos.js
+++ src/components/pages/photos/Photos.js
@@ -3,33 +3,6 @@ import photos from "./PhotosInfo.json"; // Підключення JSON-дани�
 
 const Photos = () => {
     return (
-        // <>
-        //     <h1>Photosssssssssssss!</h1>
-        //     <div className={photosStyle.item}>
-        //         <img src="https://picsum.photos/250/250?random=1" alt="random image"/>
-        //         <img src="https://picsum.photos/250/250?random=2" alt="random image"/>
-        //         <img src="https://picsum.photos/250/250?random=3" alt="random image"/>
-        //         <img src="https://picsum.photos/250/250?random=4" alt="random image"/>
-        //         <img src="https://picsum.photos/250/250?random=5" alt="random image"/>
-        //         <img src="https://picsum.photos/250/250?random=6" alt="random image"/>
-        //         <img src="https://picsum.photos/250/250?random=7" alt="random image"/>
-        //         <img src="https://picsum.photos/250/250?random=8" alt="random image"/>
-        //         <img src="https://picsum.photos/250/250?random=9" alt="random image"/>
-        //     </div>
-        // </>
-        // <>
-        //     <div className={photosStyle.photos}>
-        //         <h2 className={photosStyle.title}>Photos</h2>
-        //         <div className={photosStyle.wrapper}>
-        //             {data.map((e) => (
-        //                 <div className={photosStyle.card}>
-        //                     <img className={photosStyle.imgBlock} src={e.img} alt="" />
-        //                     <p>{e.text}</p>
-        //                 </div>
-        //             ))}
-        //         </div>
-        //     </div>
-        // </>
         <div className={photosStyle.gallery}>
             {photos.map((item, index) => (
                 <div className={photosStyle.item} key={index}>
